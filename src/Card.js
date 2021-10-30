dc9e1577b05ac5ce338663f2f36778798b5ca12c
haucong <ngoconghau65@gmail.com>
2021-10-31 00:37:38 +0700

    add 2

--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 function Card(props) {
+    const [readmore, setReadmore] = useState(false)
+
   return (
     <>
-        <div className='col-md-4 col-10 mx-auto'>
+      <div className='col-md-4 col-10 mx-auto'>
         <div className='card'>
-            <img src={props.imgsrc} className='card-img-top' alt={props.imgsrc} />
-            <div className='card-body'>
-            <h5 className='card-title font-weight-bold'>{props.title}</h5>
-            <p className='card-text'>
-                {props.des}
-            </p>
-            <NavLink to='' className='btn btn-primary'>
-                Go somewhere
-            </NavLink>
+          <img src={props.imgsrc} className='card-img-top' alt={props.imgsrc} />
+          <div className='card-body'>
+            <h2 className='card-title font-weight-bold'>{props.title}</h2>
+            <div className='description'>
+              <p className='card-text'>
+                {readmore ? props.des : `${props.des.substring(0, 100)}...`}
+              </p>
+              <button
+                className='btn mb-3 btn btn-outline-secondary'
+                onClick={() => setReadmore(!readmore)}
+              >
+                {readmore ? 'show less' : 'show more'}
+              </button>
             </div>
+
+            <NavLink to='' className='btn btn-primary'>
+              Show detail
+            </NavLink>
+          </div>
         </div>
-        </div>
+      </div>
     </>
   )
 }
